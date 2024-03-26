09a78560e2e25599b3d585ca7b98996c5a022800
Mels <mels.vagharshyan18@gmail.com>
2024-03-26 10:54:32 +0400

    yes

--- controllers/posts/create-information.js
+++ controllers/posts/create-information.js
@@ -31,7 +31,7 @@ export const deleteInformation = async (req, res) => {
   const id = req.params.id;
   try {
     await informationModel.findByIdAndDelete(id);
-    res.status(200).json("deleted successfully");
+    res.status(200).json({ message: "deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
     console.log(err);
