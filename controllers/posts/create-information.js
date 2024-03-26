6de46f97a398418b9afad099e71573585834ad96
Mels <mels.vagharshyan18@gmail.com>
2024-03-26 10:23:07 +0400

    fixed

--- controllers/posts/create-information.js
+++ controllers/posts/create-information.js
@@ -10,7 +10,7 @@ export const createInformation = async (req, res) => {
       password,
       socialSite,
     });
-    res.status(200).json(information);
+    res.status(200).json("ok");
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
     console.log(err);
@@ -21,6 +21,17 @@ export const getInformation = async (req, res) => {
   try {
     const information = await informationModel.find();
     res.status(200).json(information);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(err);
+  }
+};
+
+export const deleteInformation = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await informationModel.findByIdAndDelete(id);
+    res.status(200).json("deleted successfully");
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
     console.log(err);
--- routes/information.js
+++ routes/information.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createInformation,
+  deleteInformation,
   getInformation,
 } from "../controllers/posts/create-information.js";
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.post("/", createInformation);
 router.get("/", getInformation);
+router.delete("/delete", deleteInformation);
 
 export default router;
